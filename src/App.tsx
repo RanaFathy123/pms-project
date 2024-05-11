1998e7e9048abeeea53fb7a14e07dc52fb87dce3
RanaFathy123 <134057137+RanaFathy123@users.noreply.github.com>
2024-05-11 20:57:09 +0300

    fix deploy problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import './App.css'
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import AuthLayout from "./Modules/SharedModules/components/AuthLayout/AuthLayout";
