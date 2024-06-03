2dbea76920256d9514d6a19932963c7ffbcde569
RanaFathy123 <134057137+RanaFathy123@users.noreply.github.com>
2024-06-03 11:52:29 +0300

    enhance

--- src/Modules/SharedModules/components/MasterLayout/MasterLayout.tsx
+++ src/Modules/SharedModules/components/MasterLayout/MasterLayout.tsx
@@ -5,17 +5,20 @@ import { useState } from "react";
 
 export default function MasterLayout() {
   const [isCollapsed, setIsCollapse] = useState<boolean>(false);
+  const [toggled, setToggled] = useState(false);
 
   return (
     <>
       <div className="min-vh-100 d-flex flex-column">
-        <NavBar />
+        <NavBar  />
         <div className="container-fluid p-0 flex-grow-1 mt-5">
           <div className="d-flex ">
             <div className="min-vh-100 h-100 z-3 mt-5">
               <SideBar
                 isCollapsed={isCollapsed}
                 setIsCollapse={setIsCollapse}
+                toggled={toggled}
+                setToggled={setToggled}
               />
             </div>
             <div
--- src/Modules/SharedModules/components/NavBar/NavBar.tsx
+++ src/Modules/SharedModules/components/NavBar/NavBar.tsx
@@ -10,7 +10,7 @@ export default function NavBar() {
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top ">
         <div className="container-fluid">
-          
+     
           <Link className="navbar-brand px-5" to="">
             <img src={navlogo} alt="" />
           </Link>
